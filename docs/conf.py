43a4cbedfe021f83ebe9223945aa0c389b1d3249
Lioscro <phoenixter96@gmail.com>
2021-05-26 21:41:02 -0400

    Bump version: 1.1.2 → 1.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'ngs-tools'
 copyright = '2021, Kyung Hoi (Joseph) Min'
 author = 'Kyung Hoi (Joseph) Min'
 
-release = '1.1.2'
+release = '1.2.0'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
