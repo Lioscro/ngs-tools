8890de28d5e429d885aff7204cd16c72c4e90c2c
Lioscro <phoenixter96@gmail.com>
2021-05-31 12:32:31 -0400

    move over utils

--- ngs_tools/utils.py
+++ ngs_tools/utils.py
@@ -1,12 +1,135 @@
 import gzip
 import os
+import pickle
+import shutil
+import subprocess
 import tempfile
+import time
+import threading
 from abc import abstractmethod
-from typing import Any, Optional, TextIO
+from contextlib import contextmanager
+from operator import add
+from typing import Any, Callable, Generator, List, Optional, TextIO, Tuple
+from urllib.request import urlretrieve
 
 from joblib import Parallel
 from tqdm import tqdm
 from typing_extensions import Literal
+
+from .logging import logger
+
+
+def retry(
+    function: Callable,
+    n: int,
+    args: Optional[tuple] = None,
+    kwargs: Optional[dict] = None,
+    retry_every: Optional[int] = None,
+    backoff: bool = False,
+    exceptions: Optional[Tuple[Exception]] = None,
+):
+    """Utility function to retry a function some number of times, with optional
+    exponential backoff.
+
+    Args:
+        function: Function to retry
+        n: Number of times to retry
+        args: Function arguments
+        kwargs: Dictionary of keyword arguments
+        retry_every: Time to wait in seconds between retries. Defaults to no wait time.
+        backoff: Whether or not to exponential backoff between retries
+        exceptions: Tuple of exceptions to expect. Defaults to all exceptions.
+    """
+    args = args or tuple()
+    kwargs = kwargs or {}
+    exceptions = exceptions or Exception
+    failed = 0
+    while True:
+        try:
+            return function(*args, **kwargs)
+        except exceptions:
+            failed += 1
+            if failed >= n:
+                raise
+            if retry_every:
+                time.sleep(retry_every)
+                if backoff:
+                    retry_every *= 2
+
+
+def run_executable(
+    command: List[str],
+    stdin=None,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    wait: bool = True,
+    stream: bool = True,
+    quiet: bool = False,
+    returncode: int = 0,
+    alias: bool = True,
+) -> subprocess.Popen:
+    """Execute a single shell command.
+
+    Args:
+        command: A list representing a single shell command
+        stdin: Object to pass into the ``stdin`` argument for :class:``subprocess.Popen``.
+            Defaults to None
+        stdout: Object to pass into the `stdout` argument for :class:``subprocess.Popen``
+            Defaults to ``subprocess.PIPE``
+        stderr: Object to pass into the `stderr` argument for :class:``subprocess.Popen``,
+            Defaults to ``subprocess.PIPE``
+        wait: Whether to wait until the command has finished. Defaults to True
+        stream: Whether to stream the output to the command line. Defaults to True
+        quiet: Whether to not display anything to the command line and not check the return code.
+            Defaults to False
+        returncode: The return code expected if the command runs as intended.
+            Defaults to 0
+        alias: Whether to use the basename of the first element of `command`.
+            Defaults to `True`
+
+    Returns:
+        The spawned process
+
+    Raises:
+        subprocess.CalledProcessError: If not ``quiet`` and the process
+            exited with an exit code != ``exitcode``
+    """
+    command = [str(c) for c in command]
+    if not quiet:
+        c = command.copy()
+        if alias:
+            c[0] = os.path.basename(c[0])
+        logger.debug(' '.join(c))
+    p = subprocess.Popen(
+        command,
+        stdin=stdin,
+        stdout=stdout,
+        stderr=stderr,
+        universal_newlines=wait,
+        bufsize=1 if wait else -1,
+    )
+
+    # Wait if desired.
+    if wait:
+        out = []
+        while p.poll() is None:
+            if stream and not quiet:
+                for _line in p.stdout:
+                    line = _line.strip()
+                    out.append(line)
+                    logger.debug(line)
+                for _line in p.stderr:
+                    line = _line.strip()
+                    out.append(line)
+                    logger.debug(line)
+            else:
+                time.sleep(1)
+
+        if not quiet and p.returncode != returncode:
+            logger.error('\n'.join(out))
+            raise subprocess.CalledProcessError(p.returncode, ' '.join(command))
+
+    return p
 
 
 class ParallelWithProgress(Parallel):
@@ -64,6 +187,146 @@ def open_as_text(path: str, mode: Literal['r', 'w']) -> TextIO:
         Opened file pointer that supports ``read`` and ``write`` functions.
     """
     return gzip.open(path, f'{mode}t') if is_gzip(path) else open(path, mode)
+
+
+def decompress_gzip(gzip_path: str, out_path: str) -> str:
+    """Decompress a gzip file to provided file path.
+
+    Args:
+        gzip_path: Path to gzip file
+        out_path: Path to decompressed file
+
+    Returns:
+        Path to decompressed file
+    """
+    with gzip.open(gzip_path, 'rb') as f, open(out_path, 'wb') as out:
+        shutil.copyfileobj(f, out)
+    return out_path
+
+
+def compress_gzip(file_path: str, out_path: str) -> str:
+    """Compress a file into gzip.
+
+    Args:
+        file_path: Path to file
+        out_dir: Path to compressed file
+
+    Returns:
+        Path to compressed file
+    """
+    with open(file_path, 'rb') as f, gzip.open(out_path, 'wb') as out:
+        shutil.copyfileobj(f, out)
+    return out_path
+
+
+def concatenate_files(*paths: str, out_path: str):
+    """Concatenates an arbitrary number of files into one file.
+
+    Args:
+        *paths: An arbitrary number of paths to files
+        out_path: Path to place concatenated file
+
+    Returns:
+        Path to concatenated file
+    """
+    with open(out_path, 'wb') as out:
+        for path in paths:
+            with open(path, 'rb') as f:
+                shutil.copyfileobj(f, out)
+
+    return out_path
+
+
+def concatenate_files_as_text(*paths: str, out_path: str) -> str:
+    """Concatenates an arbitrary number of files into one TEXT file.
+
+    Only supports plaintext and gzip files.
+
+    Args:
+        *paths: An arbitrary number of paths to files
+        out_path: Path to place concatenated file
+
+    Returns:
+        Path to concatenated file
+    """
+    with open(out_path, 'w') as out:
+        for path in paths:
+            with open_as_text(path, 'r') as f:
+                for line in f:
+                    if not line.isspace():
+                        out.write(line.strip() + '\n')
+
+    return out_path
+
+
+class TqdmUpTo(tqdm):
+    """Wrapper around :func:`tqdm` so that it can be used with :func:`urlretrieve`.
+    https://github.com/tqdm/tqdm/blob/master/examples/tqdm_wget.py
+    """
+
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        return self.update(b * bsize - self.n)  # also sets self.n = b * bsize
+
+
+def download_file(url: str, path: str) -> str:
+    """Download a remote file to the provided path while displaying a progress bar.
+
+    Args:
+        url: Remote url
+        path: Local path to download the file to
+
+    Returns:
+        Path to downloaded file
+    """
+    with TqdmUpTo(unit='B', unit_scale=True, unit_divisor=1024,
+                  miniters=1) as t:
+        urlretrieve(url, filename=path, reporthook=t.update_to, data=None)
+        t.total = t.n
+    return path
+
+
+@contextmanager
+def stream_file(url: str, path: str) -> str:
+    """A context manager that creates a FIFO file to use for piping remote files
+    into processes. This function must be used as a context manager (the ``with``
+    keyword) so that any exceptions in the streaming thread may be captured.
+
+    This function spawns a new thread to download the remote file into a FIFO
+    file object. FIFO file objects are only supported on unix systems.
+
+    Args:
+        url: Url to the file
+        path: Path to place FIFO file
+
+    Yields:
+        Path to FIFO file
+
+    Raises:
+        OSError: If the operating system does not support FIFO
+
+    """
+    try:
+        os.mkfifo(path)
+    except AttributeError:
+        raise OSError(
+            'Operating system does not support streaming FIFO files. '
+            'Download the file instead.'
+        )
+    logger.debug(f'Streaming {url} to {path}')
+    t = threading.Thread(target=urlretrieve, args=(url, path), daemon=True)
+    t.start()
+    try:
+        yield path
+    finally:
+        # There are two possible cases of getting here:
+        # 1) The file has been completely streamed and processed
+        # 2) An error occurred in the with block
+        # In the case of 1), join should return immediately. In the case of 2),
+        # we need a timeout because there isn't any point in fully downloading the
+        # file.
+        t.join(timeout=10)
 
 
 def all_exists(*paths: str) -> bool:
@@ -179,3 +442,145 @@ def mkstemp(dir: Optional[str] = None, delete: bool = False):
     if delete:
         os.remove(path)
     return path
+
+
+def write_pickle(obj: object, path: str, *args, **kwargs) -> str:
+    """Pickle a Python object and compress with Gzip.
+
+    Any additional arguments and keyword arguments are passed to :func:`pickle.dump`.
+
+    Args:
+        obj: Object to pickle
+        path: Path to save pickle
+
+    Returns:
+        Saved pickle path
+    """
+    with gzip.open(path, 'wb') as f:
+        pickle.dump(obj, f, *args, **kwargs)
+    return path
+
+
+def read_pickle(path: str) -> object:
+    """Load a Python pickle that was compressed with Gzip.
+
+    Args:
+        path: Path to pickle
+
+    Returns:
+        Unpickled object
+    """
+    with gzip.open(path, 'rb') as f:
+        return pickle.load(f)
+
+
+def flatten_dictionary(
+    d: dict,
+    keys: Optional[tuple] = None
+) -> Generator[Tuple[tuple, object], None, None]:
+    """Generator that flattens the given dictionary into 2-element tuples
+    containing keys and values. For nested dictionaries, the keys are
+    appended into a tuple.
+
+    Args:
+        d: Dictionary to flatten
+        keys: Previous keys, defaults to None. Used exclusively for recursion.
+
+    Yields:
+        Flattened dictionary as (keys, value)
+    """
+    keys = keys or tuple()
+    for k, v in d.items():
+        new_keys = keys + (k,)
+        if isinstance(v, dict):
+            yield from flatten_dictionary(v, new_keys)
+        else:
+            yield new_keys, v
+
+
+def flatten_list(lst: list) -> Generator[object, None, None]:
+    """Generator that flattens the given list.
+
+    Args:
+        lst: List to flatten
+
+    Yields:
+        Flattened list elements
+    """
+    if not isinstance(lst, list):
+        yield lst
+
+    for element in lst:
+        if not isinstance(element, list):
+            yield element
+        else:
+            yield from flatten_list(element)
+
+
+def merge_dictionaries(
+    d1: dict,
+    d2: dict,
+    f: Callable[[object, object], object] = add,
+    default: object = 0
+) -> dict:
+    """Merge two dictionaries, applying an arbitrary function `f` to duplicate keys.
+    Dictionaries may be nested.
+
+    Args:
+        d1: First dictionary
+        d2: Second dictionary
+        f: Merge function. This function should take two arguments and return one,
+            defaults to `+`
+        default: Default value or callable to use for keys not present in either
+            dictionary, defaults to `0`
+
+    Returns:
+        Merged dictionary
+    """
+
+    def setdefault_nested(d, t, value):
+        inner = d
+        for k in t[:-1]:
+            inner = inner.setdefault(k, {})
+        return inner.setdefault(t[-1], value)
+
+    def get_nested(d, t, default=None):
+        inner = d
+        for k in t[:-1]:
+            inner = inner.get(k, {})
+        return inner.get(t[-1], default() if callable(default) else default)
+
+    # Extract all keys
+    d1_keys = [key for key, value in flatten_dictionary(d1)]
+    d2_keys = [key for key, value in flatten_dictionary(d2)]
+    keys = list(set(d1_keys + d2_keys))
+
+    merged = {}
+    for key in sorted(keys):
+        setdefault_nested(
+            merged, key,
+            f(get_nested(d1, key, default), get_nested(d2, key, default))
+        )
+
+    return merged
+
+
+def flatten_dict_values(d: dict) -> list:
+    """Extract all values from a nested dictionary.
+
+    Args:
+        d: Nested dictionary from which to extract values from
+
+    Returns:
+        All values from the dictionary as a list
+    """
+    if isinstance(d, dict):
+        flattened = []
+        for k, v in d.items():
+            if isinstance(v, dict):
+                flattened.extend(flatten_dict_values(v))
+            else:
+                flattened.append(v)
+        return flattened
+    else:
+        return [d]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,8 @@
+import gzip
 import os
-from unittest import TestCase
+import pickle
+import subprocess
+from unittest import mock, TestCase
 
 from joblib import delayed
 
@@ -9,6 +12,53 @@ from . import mixins
 
 
 class TestUtils(mixins.TestMixin, TestCase):
+
+    def test_retry(self):
+        test = mock.MagicMock()
+        test.return_value = 'test'
+        self.assertEqual(
+            'test', utils.retry(test, n=3, args=('a', 'b'), kwargs={'c': 'd'})
+        )
+        test.assert_called_once_with('a', 'b', c='d')
+
+    def test_retry_raises(self):
+        test = mock.MagicMock()
+        test.side_effect = Exception('test')
+        with self.assertRaises(Exception):
+            utils.retry(test, n=3)
+        self.assertEqual(3, test.call_count)
+
+    def test_run_executable(self):
+        p = utils.run_executable(['echo', 'TEST'], stream=False)
+        utils.retry(
+            self.assertEqual,
+            3,
+            args=(p.stdout.read(), 'TEST\n'),
+            retry_every=1
+        )
+
+    def test_run_exectuable_raises_exception(self):
+        with self.assertRaises(subprocess.SubprocessError):
+            utils.run_executable(['bash', 'nonexistent option'])
+
+    def test_run_exectuable_with_returncode(self):
+        utils.run_executable(['bash', 'nonexistent option'], returncode=127)
+
+    def test_run_executable_no_wait(self):
+        with mock.patch('ngs_tools.utils.subprocess') as sp_mock:
+            sp_mock.Popen().returncode = 0
+            utils.run_executable(['echo', 'TEST'], wait=False)
+            sp_mock.Popen().poll.assert_not_called()
+
+    def test_run_executable_with_stream(self):
+        with mock.patch('ngs_tools.utils.logger.debug') as debug_mock:
+            utils.run_executable(['echo', 'TEST'], stream=True)
+            utils.retry(
+                debug_mock.assert_has_calls,
+                3,
+                args=([mock.call('TEST')],),
+                retry_every=1
+            )
 
     def test_ParallelWithProgress(self):
         utils.ParallelWithProgress(
@@ -26,3 +76,161 @@ class TestUtils(mixins.TestMixin, TestCase):
     def test_mkstemp_delete(self):
         path = utils.mkstemp(delete=True)
         self.assertFalse(os.path.exists(path))
+
+    def test_decompress_gzip(self):
+        gzip_path = os.path.join(self.temp_dir, 'compressed.gz')
+        out_path = os.path.join(self.temp_dir, 'decompressed')
+        with gzip.open(gzip_path, 'wt') as f:
+            f.write('TESTING\nTEST')
+        self.assertEqual(out_path, utils.decompress_gzip(gzip_path, out_path))
+        self.assertTrue(os.path.exists(out_path))
+        with open(out_path, 'r') as f:
+            self.assertEqual('TESTING\nTEST', f.read())
+
+    def test_compress_gzip(self):
+        file_path = os.path.join(self.temp_dir, 'file')
+        out_path = os.path.join(f'{file_path}.gz')
+        with open(file_path, 'w') as f:
+            f.write('TESTING\nTEST')
+        self.assertEqual(out_path, utils.compress_gzip(file_path, out_path))
+        self.assertTrue(os.path.exists(out_path))
+        with gzip.open(out_path, 'rt') as f:
+            self.assertEqual('TESTING\nTEST', f.read())
+
+    def test_concatenate_files(self):
+        file1_path = os.path.join(self.temp_dir, 'file1')
+        file2_path = os.path.join(self.temp_dir, 'file2')
+
+        with open(file1_path, 'w') as f:
+            f.write('TEST1\n')
+        with open(file2_path, 'w') as f:
+            f.write('TEST2\n')
+
+        out_path = utils.concatenate_files(
+            file1_path,
+            file2_path,
+            out_path=os.path.join(self.temp_dir, 'concatenated'),
+        )
+
+        with open(out_path, 'r') as f:
+            self.assertEqual(f.read(), 'TEST1\nTEST2\n')
+
+    def test_concatenate_files_as_text(self):
+        file1_path = os.path.join(self.temp_dir, 'file1')
+        file2_path = os.path.join(self.temp_dir, 'file2.gz')
+
+        with open(file1_path, 'w') as f:
+            f.write('TEST1')
+        with gzip.open(file2_path, 'wt') as f:
+            f.write('TEST2')
+
+        out_path = utils.concatenate_files_as_text(
+            file1_path,
+            file2_path,
+            out_path=os.path.join(self.temp_dir, 'concatenated'),
+        )
+
+        with open(out_path, 'r') as f:
+            self.assertEqual(f.read(), 'TEST1\nTEST2\n')
+
+    def test_download_file(self):
+        with mock.patch('ngs_tools.utils.urlretrieve') as urlretrieve,\
+            mock.patch('ngs_tools.utils.TqdmUpTo', mixins.tqdm_mock):
+            path = os.path.join(self.temp_dir, 'test')
+            self.assertEqual(path, utils.download_file('remote/path', path))
+            urlretrieve.assert_called_once_with(
+                'remote/path',
+                filename=path,
+                reporthook=mock.ANY,
+                data=mock.ANY
+            )
+
+    def test_stream_file(self):
+        with mock.patch('ngs_tools.utils.os.mkfifo'),\
+            mock.patch('ngs_tools.utils.urlretrieve') as urlretrieve,\
+            mock.patch('ngs_tools.utils.threading.Thread') as Thread:
+            with utils.stream_file('remote/path', 'local/path') as path:
+                self.assertEqual('local/path', path)
+                Thread.assert_called_once_with(
+                    target=urlretrieve,
+                    args=('remote/path', 'local/path'),
+                    daemon=True
+                )
+                Thread.return_value.start.assert_called_once()
+                Thread.return_value.join.assert_not_called()
+            Thread.return_value.join.assert_called_once()
+
+    def test_stream_file_not_supported(self):
+        with mock.patch('ngs_tools.utils.os.mkfifo') as mkfifo,\
+            mock.patch('ngs_tools.utils.urlretrieve'),\
+            mock.patch('ngs_tools.utils.threading.Thread') as Thread:
+            mkfifo.side_effect = AttributeError('test')
+            with self.assertRaises(OSError):
+                with utils.stream_file('remote/path', 'local/path'):
+                    pass
+                Thread.assert_not_called()
+
+    def test_stream_file_exception(self):
+        with mock.patch('ngs_tools.utils.os.mkfifo'),\
+            mock.patch('ngs_tools.utils.urlretrieve') as urlretrieve,\
+            mock.patch('ngs_tools.utils.threading.Thread') as Thread:
+            with self.assertRaises(Exception):
+                with utils.stream_file('remote/path', 'local/path') as path:
+                    self.assertEqual('local/path', path)
+                    Thread.assert_called_once_with(
+                        target=urlretrieve,
+                        args=('remote/path', 'local/path'),
+                        daemon=True
+                    )
+                    Thread.return_value.start.assert_called_once()
+                    Thread.return_value.join.assert_not_called()
+                    raise Exception('test')
+            Thread.return_value.join.assert_called_once()
+
+    def test_write_pickle(self):
+        path = os.path.join(self.temp_dir, 'pkl')
+        utils.write_pickle('test', path)
+        with gzip.open(path, 'rb') as f:
+            self.assertEqual('test', pickle.load(f))
+
+    def test_read_pickle(self):
+        path = os.path.join(self.temp_dir, 'pkl')
+        with gzip.open(path, 'wb') as f:
+            pickle.dump('test', f)
+        self.assertEqual('test', utils.read_pickle(path))
+
+    def test_flatten_dict_values(self):
+        d = {
+            'key1': 'value1',
+            'key2': {
+                'key3': {
+                    'key4': 'value2',
+                    'key5': 'value3'
+                },
+                'key6': 'value4'
+            }
+        }
+        self.assertEqual(['value1', 'value2', 'value3', 'value4'],
+                         utils.flatten_dict_values(d))
+
+    def test_flatten_dictionary(self):
+        d = {'a': 'b', 'c': 'd', 'e': {'f': 'g', 'h': {'i': 'j'}}}
+        self.assertEqual([(('a',), 'b'), (('c',), 'd'), (('e', 'f'), 'g'),
+                          (('e', 'h', 'i'), 'j')],
+                         list(utils.flatten_dictionary(d)))
+
+    def test_flatten_list(self):
+        lst = [1, [2, 3], [4, [5, 6]]]
+        self.assertEqual([1, 2, 3, 4, 5, 6], list(utils.flatten_list(lst)))
+
+    def test_merge_dictionaries(self):
+        d1 = {'a': 'b', 'c': {'d': 'e'}, 'f': 'g'}
+        d2 = {'a': 'h', 'c': {'i': 'j'}}
+        self.assertEqual({
+            'a': 'bh',
+            'c': {
+                'd': 'eX',
+                'i': 'Xj'
+            },
+            'f': 'gX'
+        }, utils.merge_dictionaries(d1, d2, default='X'))
