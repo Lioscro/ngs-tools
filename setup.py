6c1c06fbc9382e97955bdd84cb506c9764683513
Lioscro <phoenixter96@gmail.com>
2021-07-10 11:07:14 -0400

    Bump version: 1.4.0 → 1.5.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = read('README.md')
 
 setup(
     name='ngs-tools',
-    version='1.4.0',
+    version='1.5.0',
     url='https://github.com/Lioscro/ngs-tools',
     author='Kyung Hoi (Joseph) Min',
     author_email='phoenixter96@gmail.com',
