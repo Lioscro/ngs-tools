82f831e5de573fd2ef3b9983f05bcc013b445b26
Lioscro <phoenixter96@gmail.com>
2021-07-10 11:06:41 -0400

    exclude tests.* in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     keywords='',
     python_requires='>=3.6',
     license='MIT',
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=find_packages(exclude=('tests', 'docs', 'tests.*')),
     zip_safe=False,
     include_package_data=True,
     install_requires=read('requirements.txt').strip().split('\n'),
