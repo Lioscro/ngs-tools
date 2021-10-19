c64f6e213d10aeb0488f55a7946d939fb78ce21a
Lioscro <phoenixter96@gmail.com>
2021-10-19 11:25:08 -0400

    Bump version: 1.5.7 → 1.5.8

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'ngs-tools'
 copyright = '2021, Kyung Hoi (Joseph) Min'
 author = 'Kyung Hoi (Joseph) Min'
 
-release = '1.5.7'
+release = '1.5.8'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
