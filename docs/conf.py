838fe019c17e44eb84bf8a578506a0cce9c6dec2
Lioscro <phoenixter96@gmail.com>
2022-04-20 17:25:59 -0400

    Bump version: 1.7.0 → 1.7.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'ngs-tools'
 copyright = '2021, Kyung Hoi (Joseph) Min'
 author = 'Kyung Hoi (Joseph) Min'
 
-release = '1.7.0'
+release = '1.7.1'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
--- ngs_tools/__init__.py
+++ ngs_tools/__init__.py
@@ -1,3 +1,3 @@
 from . import bam, chemistry, fasta, fastq, gtf, logging, sequence, utils  # noqa
 
-__version__ = '1.7.0'
+__version__ = '1.7.1'
